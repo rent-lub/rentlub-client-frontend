0aea22fab81306c8e3393c77dfe6421e9dc7acff
Kerkwit Supawattanakul <yongkerkwith@gmail.com>
2024-02-13 15:36:25 +0700

    custom image carousel

--- app/shoppingDetail/page.tsx
+++ app/shoppingDetail/page.tsx
@@ -7,7 +7,12 @@ const ShoppingDetailPage = () => {
   return (
     <>
       <div className="bg-white h-screen min-w-full  relative flex flex-col overflow-y-hidden">
-        <ImageCarousel />
+        <ImageCarousel
+          images={[
+            "https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/30/20/2000007885530/2000007885530_1-20231226164926-.jpg",
+            "https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/30/20/2000007885530/2000007885530_1-20231226164926-.jpg",
+          ]}
+        />
       </div>
     </>
   );
--- components/shoppingDetail/imageCarousel.tsx
+++ components/shoppingDetail/imageCarousel.tsx
@@ -1,59 +1,43 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-const ImageCarousel = () => {
+interface ImageCarouselProps {
+  images: String[];
+}
+
+const ImageCarousel: React.FC<ImageCarouselProps> = ({ images, ...props }) => {
+  const [currentImage, setCurrentImage] = useState<number>(0);
+  var settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    afterChange: (current: number) => setCurrentImage(current),
+  };
   return (
     <>
-      <Swiper
-        pagination={{
-          type: "fraction",
-        }}
-        navigation={true}
-        modules={[Pagination, Navigation]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <Image
-            src="https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/30/20/2000007885530/2000007885530_1-20231226164926-.jpg"
-            width={500}
-            height={500}
-            alt="Product"
-            className="rounded-xl object-contain"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image
-            src="https://resource.nationtv.tv/uploads/images/md/2022/03/1bEPuf4wZD8ToCld5jcp.webp?x-image-process=style/lg"
-            width={500}
-            height={500}
-            alt="Product"
-            className="rounded-xl object-contain"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image
-            src="https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/30/20/2000007885530/2000007885530_1-20231226164926-.jpg"
-            width={500}
-            height={500}
-            alt="Product"
-            className="rounded-xl object-contain"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image
-            src="https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/30/20/2000007885530/2000007885530_1-20231226164926-.jpg"
-            width={500}
-            height={500}
-            alt="Product"
-            className="rounded-xl object-contain"
-          />
-        </SwiperSlide>
-      </Swiper>
+      <div className="relative">
+        <Slider {...settings} adaptiveHeight arrows>
+          {images.map((item, index) => (
+            <div key={item[index]}>
+              <Image
+                width={500}
+                height={500}
+                src={
+                  "https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/30/20/2000007885530/2000007885530_1-20231226164926-.jpg"
+                }
+                alt="image"
+              />
+            </div>
+          ))}
+        </Slider>
+        <div className="absolute right-10 bottom-10 text-white bg-black bg-opacity-70 px-4 py-1 text-sm rounded-xl">
+          {currentImage + 1}/{images.length}
+        </div>
+      </div>
     </>
   );
 };
