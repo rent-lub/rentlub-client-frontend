d31cb1f0ee4281580784d8793ea6f10916a795f1
Kerkwit Supawattanakul <yongkerkwith@gmail.com>
2024-02-14 23:53:35 +0700

    compiler style component true

--- app/StoreProvider.tsx
+++ app/StoreProvider.tsx
@@ -3,7 +3,6 @@ import { useRef } from "react";
 import { Provider } from "react-redux";
 import { makeStore, AppStore } from "../lib/store";
 import { NextUIProvider } from "@nextui-org/react";
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 
 export default function StoreProvider({
   children,
@@ -16,10 +15,8 @@ export default function StoreProvider({
   }
 
   return (
-    <AppRouterCacheProvider>
-      <NextUIProvider>
-        <Provider store={storeRef.current}>{children}</Provider>
-      </NextUIProvider>
-    </AppRouterCacheProvider>
+    <NextUIProvider>
+      <Provider store={storeRef.current}>{children}</Provider>
+    </NextUIProvider>
   );
 }
