2c921dc14c95fabbf2b9ddde851af66024c24704
Kerkwit Supawattanakul <yongkerkwith@gmail.com>
2024-03-17 21:47:43 +0700

    custom calendar for reserveation

--- components/customCalendar.tsx
+++ components/customCalendar.tsx
@@ -1,4 +1,13 @@
 import React, { useState } from "react";
+import { CalendarLabelEnum } from "~/types/calendarLabelEnum";
+import { DateTime } from "luxon";
+
+interface ReservationItem {
+  label?: CalendarLabelEnum;
+  startDate: string;
+  endDate: string;
+  item: string;
+}
 
 interface CalendarProps {
   onDateSelect: (selectedDate: Date) => void;
@@ -7,6 +16,21 @@ interface CalendarProps {
 const CustomCalendar: React.FC<CalendarProps> = ({ onDateSelect }) => {
   const currentDate: Date = new Date();
   const [date, setDate] = useState<Date>(currentDate);
+
+  const reserveItemsList: Array<ReservationItem> = [
+    {
+      item: "first item",
+      startDate: "3/22/2024",
+      endDate: "3/24/2024",
+      label: CalendarLabelEnum.Unavailable,
+    },
+    {
+      item: "second item",
+      startDate: "3/17/2024",
+      endDate: "3/20/2024",
+      label: CalendarLabelEnum.Unavailable,
+    },
+  ];
 
   const getDaysInMonth = (date: Date): number => {
     const year: number = date.getFullYear();
@@ -32,10 +56,45 @@ const CustomCalendar: React.FC<CalendarProps> = ({ onDateSelect }) => {
 
     for (let day = 1; day <= daysInMonth; day++) {
       const currentDate = new Date(date.getFullYear(), date.getMonth(), day);
+      // console.log(currentDate.toLocaleDateString());
+      let isStart = false;
+      let isEnd = false;
+      let isReserved = false;
+
+      for (const item of reserveItemsList) {
+        if (item.label == CalendarLabelEnum.Unavailable) {
+          const startDate = new Date(item.startDate);
+          const endDate = new Date(item.endDate);
+
+          if (currentDate.getTime() === startDate.getTime()) isStart = true;
+
+          if (currentDate.getTime() === endDate.getTime()) isEnd = true;
+
+          if (currentDate >= startDate && currentDate <= endDate) {
+            isReserved = true;
+            break;
+          }
+        }
+      }
+      console.log(
+        `${currentDate.toLocaleDateString()} == ${DateTime.now().toFormat(
+          "M/d/yyyy"
+        )}`
+      );
       currentWeek.push(
         <td
           key={day}
-          className="calendar-day text-right"
+          className={`calendar-day text-center h-7 ${
+            isReserved
+              ? `${
+                  currentDate.toLocaleDateString() ==
+                  DateTime.now().toFormat("M/d/yyyy")
+                    ? "bg-[#2995e2]"
+                    : "bg-[#C5D6E3]"
+                }  ${isStart ? `rounded-l-xl ` : isEnd ? "rounded-r-xl" : ""} `
+              : ""
+          }
+          `}
           onClick={() => {
             console.log(currentDate);
           }}
@@ -57,7 +116,7 @@ const CustomCalendar: React.FC<CalendarProps> = ({ onDateSelect }) => {
   };
 
   return (
-    <div>
+    <div className="flex flex-col">
       <div className="flex justify-between px-5">
         <button
           onClick={() =>
@@ -86,16 +145,16 @@ const CustomCalendar: React.FC<CalendarProps> = ({ onDateSelect }) => {
           {">"}
         </button>
       </div>
-      <table className="border-separate border-spacing-y-4 border-spacing-x-4">
+      <table className="border-separate border-spacing-y-3 text-center">
         <thead>
           <tr>
-            <th>Sun</th>
-            <th>Mon</th>
-            <th>Tue</th>
-            <th>Wed</th>
-            <th>Thu</th>
-            <th>Fri</th>
-            <th>Sat</th>
+            <td className="w-12">Sun</td>
+            <td className="w-12">Mon</td>
+            <td className="w-12">Tue</td>
+            <td className="w-12">Wed</td>
+            <td className="w-12">Thu</td>
+            <td className="w-12">Fri</td>
+            <td className="w-12">Sat</td>
           </tr>
         </thead>
         <tbody>{renderCalendarWeeks()}</tbody>
