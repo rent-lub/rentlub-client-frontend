2476d845e1bc660940bd6dd1ebf0d0058a286623
yongggnd <yongkerkwith@gmail.com>
2024-03-20 12:31:38 +0700

    init liff

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,36 @@
+"use client";
+import { useEffect, useState } from "react";
 import Shopping from "./shopping/pages";
+import {
+  getUserDisplayName,
+  getUserId,
+  getUserProfileImage,
+  useLiff,
+} from "~/services/liffService";
 
 export default function Home() {
+  const liff = useLiff();
+
+  const [profileImage, setProfileImage] = useState<string | null>(null);
+  const [userId, setUserId] = useState<string | null>(null);
+  const [displayName, setDisplayName] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const image = await getUserProfileImage();
+      const id = await getUserId();
+      const name = await getUserDisplayName();
+
+      setProfileImage(image);
+      setUserId(id);
+      setDisplayName(name);
+    }
+
+    fetchData();
+
+    console.log(profileImage);
+  }, []);
+
   return (
     <main className="flex min-h-screen">
       <Shopping />
