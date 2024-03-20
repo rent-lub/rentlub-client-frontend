7ac15f8e7bbe001d1361b82d5b97e38f5ae5a37f
yongggnd <yongkerkwith@gmail.com>
2024-03-20 14:27:48 +0700

    testing custom hook for liff

--- services/liffService.ts
+++ services/liffService.ts
@@ -1,34 +1,43 @@
 import { useEffect, useState } from "react";
 import liff from "@line/liff";
 
-let initialized = false;
+let liffInstance: typeof liff | null = null;
+let initPromise: Promise<typeof liff> | null = null;
 
-async function initLiff() {
-  if (!initialized) {
-    try {
-      const LIFFID = "2003536696-gwwkQzpx";
-      await liff.init({ liffId: LIFFID });
-      initialized = true;
-    } catch (error) {
-      console.error("Error initializing LIFF:", error);
+export function useLiff() {
+  const [initialized, setInitialized] = useState(false);
+
+  useEffect(() => {
+    if (!initPromise) {
+      initPromise = initLiff();
     }
+
+    initPromise.then((instance) => {
+      liffInstance = instance;
+      setInitialized(true);
+    });
+
+    return () => {
+      initPromise = null;
+    };
+  }, []);
+
+  return initialized ? liffInstance : null;
+}
+
+async function initLiff(): Promise<typeof liff> {
+  try {
+    const LIFFID = "2003536696-gwwkQzpx";
+    await liff.init({ liffId: LIFFID });
+    return liff;
+  } catch (error) {
+    console.error("Error initializing LIFF:", error);
+    throw error;
   }
 }
 
-export function useLiff() {
-  const [liffInstance, setLiffInstance] = useState<typeof liff | null>(null);
-
-  useEffect(() => {
-    initLiff().then(() => {
-      setLiffInstance(liff);
-    });
-  }, []);
-
-  return liffInstance;
-}
-
 export async function getUserProfileImage(): Promise<string | null> {
-  const liff = useLiff();
+  const liff = liffInstance || (await useLiff());
 
   if (!liff || !liff.isLoggedIn()) {
     return null;
@@ -39,7 +48,7 @@ export async function getUserProfileImage(): Promise<string | null> {
 }
 
 export async function getUserId(): Promise<string | null> {
-  const liff = useLiff();
+  const liff = liffInstance || (await useLiff());
 
   if (!liff || !liff.isLoggedIn()) {
     return null;
@@ -50,7 +59,7 @@ export async function getUserId(): Promise<string | null> {
 }
 
 export async function getUserDisplayName(): Promise<string | null> {
-  const liff = useLiff();
+  const liff = liffInstance || (await useLiff());
 
   if (!liff || !liff.isLoggedIn()) {
     return null;
