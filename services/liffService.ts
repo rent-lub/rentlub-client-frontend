e93ce73ad95c01891dc302889332ae8680bdf376
yongggnd <yongkerkwith@gmail.com>
2024-03-25 01:42:20 +0700

    implement liff service

--- services/liffService.ts
+++ services/liffService.ts
@@ -1,81 +1,76 @@
-"use client";
-
-import { useEffect, useState } from "react";
 import liff from "@line/liff";
+import { useEffect, useState } from "react";
 
-export async function initLiff() {
+let liffInstance: typeof liff | null = null;
+
+export const initLiff = async (): Promise<typeof liff | null> => {
   try {
-    const LIFFID = process.env.NEXT_PUBLIC_LIFF_IF;
-    await liff.init({ liffId: LIFFID! });
-    return liff;
+    if (!liffInstance) {
+      const LIFFID = process.env.NEXT_PUBLIC_LIFF_IF;
+      await liff.init({ liffId: LIFFID! });
+      liffInstance = liff;
+    }
+    return liffInstance;
   } catch (error) {
     console.error("Error initializing LIFF:", error);
     return null;
   }
-}
+};
 
-export function useLiff() {
-  const [liffInstance, setLiffInstance] = useState<typeof liff | null>(null);
+export const useLiff = () => {
+  const [liffState, setLiffState] = useState<typeof liff | null>(null);
 
   useEffect(() => {
     initLiff().then((liff) => {
-      setLiffInstance(liff);
+      setLiffState(liff);
     });
   }, []);
 
-  return liffInstance;
-}
+  return liffState;
+};
 
-export async function getUserProfileImage(): Promise<string | null> {
+export const getUserProfileImage = async (): Promise<string | null> => {
   const liff = await initLiff();
   if (!liff) return null;
-
   if (!liff.isLoggedIn()) {
     liff.login();
   }
-
   const profile = await liff.getProfile();
   return profile.pictureUrl ?? "";
-}
+};
 
-export async function getUserId(): Promise<string | null> {
+export const getUserId = async (): Promise<string | null> => {
   const liff = await initLiff();
   if (!liff) return null;
-
   if (!liff.isLoggedIn()) {
     liff.login();
   }
-
   const profile = await liff.getProfile();
   return profile.userId;
-}
+};
 
-export async function getUserDisplayName(): Promise<string | null> {
+export const getUserDisplayName = async (): Promise<string | null> => {
   const liff = await initLiff();
   if (!liff) return null;
-
   if (!liff.isLoggedIn()) {
     liff.login();
   }
-
   const profile = await liff.getProfile();
   return profile.displayName;
-}
+};
 
-export async function getUserToken(): Promise<string | null> {
+export const getUserToken = async (): Promise<string | null> => {
   const liff = await initLiff();
   if (!liff || !liff.isLoggedIn()) {
     return null;
   }
-
   return liff.getIDToken();
-}
+};
 
-export async function getAccessToken(): Promise<string | null> {
+export const getAccessToken = async (): Promise<string | null> => {
   const liff = await initLiff();
   if (!liff || !liff.isLoggedIn()) {
     return null;
   }
-
   return liff.getAccessToken();
-}
+};
