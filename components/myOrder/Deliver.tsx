1bc67f39352cd612e1080c92897678d9e07a8156
qxntm <siammo2002@gmail.com>
2024-03-24 23:45:36 +0700

    add link of card to orderDetail

--- components/myOrder/Deliver.tsx
+++ components/myOrder/Deliver.tsx
@@ -8,7 +8,9 @@ const Deliver= ({}) => {
           <div className="mx-5">
               <h1 className="font-medium text-xl mb-3">On My Way</h1>
               <p className="mb-2">ได้รับสินค้าแล้ว</p>
-              <MyOrderCard />
+              <a href="/orderDetail/">
+                <MyOrderCard />
+              </a> 
               <div className="mb-2 rounded-xl w-full border border-slate-200 "></div>
               <p className="mb-2">สินค้ากำลังส่ง</p>
               <p className="text-slate-400 text-sm">ไม่มี</p>
--- components/myOrder/Return.tsx
+++ components/myOrder/Return.tsx
@@ -8,7 +8,9 @@ const Return= ({}) => {
           <div className="mx-5">
               <h1 className="font-medium text-xl mb-3">Going Back</h1>
               <p className="mb-2">ครบกำหนดคืน</p>
-              <MyOrderCard />
+              <a href="/orderDetail/">
+                <MyOrderCard />
+              </a>
               <div className="mb-2 rounded-xl w-full border border-slate-200 "></div>
               <p className="mb-2">กำลังส่งคืนและรอยืนยัน</p>
               <p className="text-slate-400 text-sm">ไม่มี</p>
