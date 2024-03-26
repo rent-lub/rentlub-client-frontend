429bfeb72f6c876189967a58b46622978b384c80
Kerkwit Supawattanakul <yongkerkwith@gmail.com>
2024-03-26 19:18:12 +0700

    feat custom font

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,37 @@ import { Inter } from "next/font/google";
 import "~/styles/globals.css";
 import StoreProvider from "./StoreProvider";
 import "rsuite/dist/rsuite-no-reset.min.css";
+import localFont from "next/font/local";
 
-const inter = Inter({ subsets: ["latin"] });
+const lineSeed = localFont({
+  src: [
+    {
+      path: "../fonts/LINESeedSansTH_W_Th.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../fonts/LINESeedSansTH_W_Rg.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../fonts/LINESeedSansTH_W_Bd.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../fonts/LINESeedSansTH_W_XBd.woff2",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../fonts/LINESeedSansTH_W_He.woff2",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+});
 
 export const metadata: Metadata = {
   title: "RentLub",
@@ -17,9 +46,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${lineSeed.className}`}>
         <StoreProvider>{children}</StoreProvider>
       </body>
     </html>
   );
 }
+
+
