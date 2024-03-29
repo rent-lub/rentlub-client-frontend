1c4216ce12fa1e4ffd2ae8588d3d8e92c3df9b44
Kerkwit Supawattanakul <yongkerkwith@gmail.com>
2024-03-29 13:38:19 +0700

    return empty state

--- components/myOrder/Return.tsx
+++ components/myOrder/Return.tsx
@@ -15,28 +15,37 @@ const Return = ({}) => {
       <div className="mx-5">
         <h1 className="font-medium text-xl mb-1">Going Back</h1>
         <p className="mb-2">สินค้าที่กำลังถูกส่งกลับคืนร้าน</p>
-        {myOrder.allOrder?.length > 0 ? (
+        {myOrder.allOrder.filter(
+          (item) => item.renting.status === RentingStatus.RETURNED
+        ).length > 0 ? (
           myOrder.allOrder
             .filter((item) => item.renting.status === RentingStatus.RETURNED)
             .map((item) => (
               <React.Fragment key={item.renting._id}>
                 <div
-                    onClick={() => {
-                      var index = myOrder.allOrder.indexOf(item);
-                      rounter.push(`/orderDetail/${index}`);
-                    }}
-                  >
-                    <MyOrderCard rentItem={item} />
-                  </div>
+                  onClick={() => {
+                    var index = myOrder.allOrder.indexOf(item);
+                    rounter.push(`/orderDetail/${index}`);
+                  }}
+                >
+                  <MyOrderCard rentItem={item} />
+                </div>
               </React.Fragment>
             ))
-        )
-            :  (
-              <div className="flex flex-col items-center gap-2 pt-20">
-                <Image src="/return.svg" alt="" width={200} height={200} className="opacity-65 mr-5" />
-                <div className="text-slate-400 text-sm">ไม่พบสินค้าที่กำลังส่งกลับไปยังร้าน</div>
-              </div>
-            )}
+        ) : (
+          <div className="flex flex-col items-center gap-2 pt-20">
+            <Image
+              src="/return.svg"
+              alt=""
+              width={200}
+              height={200}
+              className="opacity-65 mr-5"
+            />
+            <div className="text-slate-400 text-sm">
+              ไม่พบสินค้าที่กำลังส่งกลับไปยังร้าน
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
